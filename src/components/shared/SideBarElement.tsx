7da267a2b9a49d678dae17ca5066eda169044657
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-05-01 17:09:38 -0500

    Iconos cambiados

--- src/components/shared/SideBarElement.tsx
+++ src/components/shared/SideBarElement.tsx
@@ -10,7 +10,7 @@ const SideBarElement = ({ IconTSX, route, text }: PageApp) => {
   return (
     <Link href={route}>
       <li  className={`flex items-center pl-5 pr-8 h-[4.2rem] gap-x-4 ${!isSelected && "hover:bg-gray-200"} ${isSelected && "bg-black"}`} title={text}>
-        <IconTSX className="aspect-auto w-11" fillColor={isSelected ? "#fff" : "black"} />
+        <IconTSX className="aspect-auto w-8" fillColor={isSelected ? "#fff" : "black"} />
         <span className={`w-max  text-[0.95rem] ${isSelected && "text-white"} `}>{text}</span>
       </li>
     </Link>
