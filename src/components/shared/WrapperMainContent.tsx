2fbdd5eaa6951f27c7161f81735d3b4712a7a188
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-05-04 00:13:36 -0500

    Sidebar estatico

---
+++ src/components/shared/WrapperMainContent.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ReactNode } from "react";
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+
+const WrapperMainContent = ({
+  children,
+}: {
+  children: ReactNode | ReactNode[];
+}) => {
+  const headerHeight = useSelector(
+    (state: RootState) => state.elementsDimensions.headerHeight
+  );
+  const windowHeight = useSelector(
+    (state: RootState) => state.elementsDimensions.windowHeight
+  );
+
+  return (
+    <>
+      <div
+        id="main-content"
+        className=" w-full flex items-start justify-center p-8"
+      >
+        {children}
+      </div>
+      <style>{`
+      
+        #main-content{            
+            min-height: ${windowHeight - headerHeight}px;
+        }
+      
+      `}</style>
+    </>
+  );
+};
+
+export default WrapperMainContent;
