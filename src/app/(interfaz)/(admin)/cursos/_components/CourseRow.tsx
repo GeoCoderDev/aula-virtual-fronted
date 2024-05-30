7934111e7d19e939235b6b9d12ee01c5ee837089
Andry Emilio Diego <103266212+Limense@users.noreply.github.com>
2024-05-30 12:21:49 -0500

    CursoBoton

--- src/app/(interfaz)/(admin)/cursos/_components/CourseRow.tsx
+++ src/app/(interfaz)/(admin)/cursos/_components/CourseRow.tsx
@@ -2,15 +2,20 @@ import { Course } from "@/interfaces/Course";
 
 const CourseRow = ({ course }: { course: Course }) => {
   return (
-    <tr>
-      <td className="text-center">{course.Id_Curso}</td>
-      <td className="text-center">{course.Nombre_Curso}</td>
-      <td className="text-center">{course.Grados}</td>
+    <tr className="-border-2">
+      <td className="p-4 text-center">{course.Id_Curso}</td>
+      <td className="p-4 text-center">{course.Nombre_Curso}</td>
+      <td className="p-4 text-center">{course.Grados}</td>
       <td className="flex justify-evenly">
-        <button className="edition-button">Editar</button>
-        <button className="deshabilitar-habilitar-button bg-rojo-orange">
-          Deshabilitar
-        </button>
+      <td>
+        <div className="w-full flex justify-center gap-x-4">
+          <button className="edition-button">Editar</button>
+          <button className="deshabilitar-habilitar-button bg-rojo-orange">
+            Deshabilitar
+          </button>
+        </div>
+      </td>
+      
       </td>
     </tr>
   );
