ae8735eb85c57d098d57e7ae1f24c71bda9b7d00
GuevSus <jesusguevar2003@gmail.com>
2024-06-30 10:27:28 -0500

    He añadido modales en las configuraciones: Interfaz y Backup

--- src/app/(interfaz)/(admin)/(superadmin)/configuraciones/interfaz/page.tsx
+++ src/app/(interfaz)/(admin)/(superadmin)/configuraciones/interfaz/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+import Link from 'next/link';
+import React, { useState } from 'react';  // Asegúrate de importar useState
+import EditarIcon from '@/components/icons/others/EditarIcon';
+import ChangeInterfazColor from "@/components/shared/modals/Configuraciones/ChangeInterfazColor";  // Asegúrate de que el import sea correcto
 
 const Interfaz = () => {
-  return (
-    <div>Interfaz</div>
-  )
-}
+  const [viewChangeInterfazColor, setViewChangeInterfazColor] = useState(false);  // Cambié a `viewChangeInterfazColor` para reflejar el estado correcto
 
-export default Interfaz
+  return (
+    <>
+      <div className="p-8">
+        <div className="flex">
+          <Link href="/configuraciones">
+            <div className="cursor-pointer hover:underline">Configuraciones</div>
+          </Link>
+          &nbsp;&gt;&nbsp;
+          <Link href="/configuraciones/backups">
+            <div className="cursor-pointer hover:underline">Interfaz</div>
+          </Link>
+        </div>
+        <div className="mb-8">
+          <h2 className="text-2xl font-semibold">Color</h2>
+          <p className="mt-2 mb-4">
+            Personaliza el color de la interfaz de usuario de la aplicación. Puedes elegir entre una variedad de opciones de color predefinidas o seleccionar un color personalizado para adaptarlo a tus preferencias visuales.
+          </p>
+          <div className="flex items-center gap-4 mb-4">
+            <span className="text-xl font-semibold">Color Actual:</span>
+            <div className="w-8 h-8 bg-green-500"></div>
+            <span className="bg-gray-300 px-4 py-2 rounded-md text-xl">#06FC68</span>
+            <button
+              className="button-with-loader ml-0"
+              onClick={() => {
+                setViewChangeInterfazColor(true);
+              }}
+            >
+              Modificar <EditarIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+      {viewChangeInterfazColor && (
+        <ChangeInterfazColor
+          eliminateModal={() => {
+            setViewChangeInterfazColor(false);
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default Interfaz;
--- src/app/(interfaz)/(admin)/(superadmin)/configuraciones/page.tsx
+++ src/app/(interfaz)/(admin)/(superadmin)/configuraciones/page.tsx
@@ -1,6 +1,27 @@
+
+
 const GestionBackups = () => {
-  return <div>Aca se podran gestionar los Backups</div>;
-  
+  return (
+    <div>
+      <div>
+        <div>
+          <span className="text-xl font-semibold">
+          <a href="configuraciones/interfaz">Interfaz</a>
+          </span>
+        </div>
+        <div>
+          <span className="text-xl font-semibold">
+          <a href="configuraciones/horario-calendario">Horario y Calendario</a>
+          </span>
+        </div>
+        <div>
+          <span className="text-xl font-semibold">
+          <a href="configuraciones/backups">Backups</a>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default GestionBackups;
