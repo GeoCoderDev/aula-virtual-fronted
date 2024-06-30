29e1661ac76af604f43fd154fa44bdcb5cf58b58
GuevSus <jesusguevar2003@gmail.com>
2024-06-30 15:01:22 -0500

    He añadido Iconos a la interfaz principal de confirguraciones para la redirección a las otras interfaces

--- src/app/(interfaz)/(admin)/(superadmin)/configuraciones/page.tsx
+++ src/app/(interfaz)/(admin)/(superadmin)/configuraciones/page.tsx
@@ -1,23 +1,27 @@
-
-
+import InterfazIcon from "@/components/icons/others/InterfazIcon";
+import BackupIcon from "@/components/icons/others/BackupIcon";
+import ScheduleIcon from "@/components/icons/others/ScheduleIcon";
 const GestionBackups = () => {
   return (
-    <div>
-      <div>
+    <div className="flex justify-center items-center h-screen w-full overflow-hidden">
+      <div className="flex space-x-20 text-center">
         <div>
-          <span className="text-xl font-semibold">
-          <a href="configuraciones/interfaz">Interfaz</a>
-          </span>
+          <a href="/configuraciones/interfaz" className="text-xl font-semibold flex flex-col items-center">
+            <InterfazIcon className="mb-2" />
+            <span>Interfaz</span>
+          </a>
         </div>
         <div>
-          <span className="text-xl font-semibold">
-          <a href="configuraciones/horario-calendario">Horario y Calendario</a>
-          </span>
+          <a href="/configuraciones/horario-calendario" className="text-xl font-semibold flex flex-col items-center">
+            <ScheduleIcon className="mb-2" />
+            <span>Horario y Calendario</span>
+          </a>
         </div>
         <div>
-          <span className="text-xl font-semibold">
-          <a href="configuraciones/backups">Backups</a>
-          </span>
+          <a href="/configuraciones/backups" className="text-xl font-semibold flex flex-col items-center">
+            <BackupIcon className="mb-2" />
+            <span>Backups</span>
+          </a>
         </div>
       </div>
     </div>
