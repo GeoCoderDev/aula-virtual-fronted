0ca961e5aa01a49f62fc915f3a5cb53478c4b49c
tavoofg <gustavo.08.alonso@gmail.com>
2024-07-02 10:31:47 -0500

    ACtualizacion sin calificaciones

--- src/app/(interfaz)/(users)/(teacher)/reportes/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Reportes = () => {
-  return (
-    <div>Aqui los profesores podra ver el desempeño de sus estudiantes</div>
-  );
-};
-
-export default Reportes;
